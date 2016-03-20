77d8f0e31ac6d9903b20443d9799745f81a6dc25
Paul Kubala <paulisdaman@gmail.com>
2016-03-20 17:29:05 -0600

    adding the base particles

--- ThreeJS/IntroParticles/js/particles.js
+++ ThreeJS/IntroParticles/js/particles.js
@@ -1,67 +1,31 @@
+// Create Particle Count, GeoMetry, and Material
+var particleCount = 1800,
+    particles = new THREE.Geometry(),
+    pMaterial = new THREE.ParticleBasicMaterial({
+      color: 0xFFFFFF,
+      size: 20
+    });
 
-	// set the scene size
-	var WIDTH = 400,
-	    HEIGHT = 300;
 
-	// set some camera attributes
-	var VIEW_ANGLE = 45,
-	    ASPECT = WIDTH / HEIGHT,
-	    NEAR = 0.1,
-	    FAR = 10000;
+for (var p = 0; p < particleCount; p++) {
 
-	// get the DOM element to attach to
-	// - assume we've got jQuery to hand
-	var $container = $('#container');
+// create a particle with random
+// position values, -250 -> 250
+	var pX = Math.random() * 500 - 250,
+	pY = Math.random() * 500 - 250,
+	pZ = Math.random() * 500 - 250,
+	particle = new THREE.Vertex(
+		new THREE.Vector3(pX, pY, pZ)
+	);
 
-	// create a WebGL renderer, camera
-	// and a scene
-	var renderer = new THREE.WebGLRenderer();
-	var camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-	                                ASPECT,
-	                                NEAR,
-	                                FAR  );
-	var scene = new THREE.Scene();
+	// add it to the geometry
+	particles.vertices.push(particle);
+}
 
-	// the camera starts at 0,0,0 so pull it back
-	camera.position.z = 300;
+// create the particle system
+var particleSystem = new THREE.ParticleSystem(
+    particles,
+    pMaterial);
 
-	// start the renderer
-	renderer.setSize(WIDTH, HEIGHT);
-
-	// attach the render-supplied DOM element
-	$container.append(renderer.domElement);
-
-	// create the sphere's material
-	var sphereMaterial = new THREE.MeshLambertMaterial(
-	{
-	    color: 0xCC0000
-	});
-
-	// set up the sphere vars
-	var radius = 50, segments = 16, rings = 16;
-
-	// create a new mesh with sphere geometry -
-	// we will cover the sphereMaterial next!
-	var sphere = new THREE.Mesh(
-	   new THREE.SphereGeometry(radius, segments, rings),
-	   sphereMaterial);
-
-	// add the sphere to the scene
-	scene.add(sphere);
-
-	// and the camera
-	scene.add(camera);
-
-	// create a point light
-	var pointLight = new THREE.PointLight( 0xFFFFFF );
-
-	// set its position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
-
-	// add to the scene
-	scene.add(pointLight);
-
-	// draw!
-	renderer.render(scene, camera);
+// add it to the scene
+scene.addChild(particleSystem);
